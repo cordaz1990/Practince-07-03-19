ef99eb12f85521eed4bfbe28599a68bd241881ad
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-13 17:56:33 -0800

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,3 +1,9 @@
-print("hello")
-print("world)
-print("m")
+def print_lyrics():
+    print("I'm a lumberjack, and I'm okay.")
+    print("I sleep all night and I work all day.")
+    
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+    
+repeat_lyrics()
